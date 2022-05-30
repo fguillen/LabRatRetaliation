4b4178df513dee7cc8230f9eb2f842b50db71584
Fernando Guillen <fguillen.mail@gmail.com>
2022-05-30 21:35:36 +0200

    MapObjects Spawn and move

--- Assets/MapObjectController.cs
+++ Assets/MapObjectController.cs
@@ -4,38 +4,42 @@ using UnityEngine;
 
 public class MapObjectController : MonoBehaviour
 {
+    [SerializeField] float speed;
+
     int screenUnits = 12;
     Vector2 originalPosition;
     int screenIndex;
     float positionXRight;
     float positionXLeft;
     bool alive;
+    int direction;
 
     void Awake()
     {
         originalPosition = transform.position;
         screenIndex = Mathf.FloorToInt(originalPosition.x / screenUnits);
-        positionXLeft = (screenIndex * screenUnits) - 100;
-        positionXLeft = (screenIndex * screenUnits) + screenUnits + 100;
+        positionXLeft = (screenIndex * screenUnits) - 1;
+        positionXRight = (screenIndex * screenUnits) + screenUnits + 1;
 
         alive = false;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        if(alive)
+            transform.position = new Vector2(transform.position.x + (direction * speed * Time.deltaTime), transform.position.y);
     }
 
-    public void Spawn(string position)
+    public void Spawn(string side)
     {
-        float positionX = position == "left" ? positionXLeft : positionXRight;
+        float positionX = side == "left" ? positionXLeft : positionXRight;
+        direction = side == "left" ? 1 : -1;
         transform.position = new Vector2(positionX, transform.position.y);
+        alive = true;
+    }
+
+    public void Stop()
+    {
+        alive = false;
     }
 }
--- Assets/ScreensController.cs
+++ Assets/ScreensController.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System;
 
 public class ScreenController
 {
     List<MapObjectController> mapObjects;
+    public int ScreenIndex { get; private set; }
 
-
-    public ScreenController(int index, GameObject[] _mapObjects)
+    public ScreenController(int screenIndex, List<MapObjectController> mapObjects)
     {
-        foreach (var mapObject in _mapObjects)
-        {
-            mapObjects.Add(mapObject.GetComponent<MapObjectController>());
-        }
+        Debug.Log($"XXX: Creating ScreenController. index: {screenIndex}, numObjects: {mapObjects.Count()}");
+        this.mapObjects = mapObjects;
+        this.ScreenIndex = screenIndex;
     }
 
     public void StartMapObjects(string position)
@@ -29,33 +29,79 @@ public class ScreenController
 
 public class ScreensController : MonoBehaviour
 {
-    int numScreens = 8;
-    GameObject[] mapObjects;
-    List<ScreenController> screenControllers;
+    [SerializeField] Transform playerTransform;
 
+    int numScreens = 8;
+    int screenUnits = 12;
+    int actualScreenIndex;
+
+    List<ScreenController> screenControllers = new List<ScreenController>();
 
     void Awake()
     {
-        mapObjects = GameObject.FindGameObjectsWithTag("MapObject");
+        BuildScreenControllers();
+        actualScreenIndex = -1;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        CheckForScreenChange();
     }
 
-    void BuildScreenControllers(GameObject[] mapObjects)
+    void CheckForScreenChange()
     {
+        int playerScreenIndex = Mathf.FloorToInt(playerTransform.position.x / screenUnits);
+        if(playerScreenIndex != actualScreenIndex)
+        {
+            Debug.Log($"XXX: newScreen: {playerScreenIndex}");
+            var playerSide = PlayerScreenOppositeSide(playerScreenIndex, playerTransform.position.x);
+            ActivateObjectsInScreenByIndex(playerSide, playerScreenIndex);
+            actualScreenIndex = playerScreenIndex;
+        }
+    }
+
+    string PlayerScreenOppositeSide(int screenIndex, float playerPositionX)
+    {
+        var screenCenterPosition = ((screenIndex * screenUnits) + (screenUnits / 2));
+        if(playerTransform.transform.position.x < screenCenterPosition)
+            return "right";
+        else
+            return "left";
+    }
+
+    void BuildScreenControllers()
+    {
+        GameObject[] mapObjectsInArray = GameObject.FindGameObjectsWithTag("MapObject");
+
+        List<MapObjectController> mapObjects = new List<MapObjectController>();
+        foreach (var item in mapObjectsInArray)
+        {
+            mapObjects.Add(item.GetComponent<MapObjectController>());
+        }
+
+        Debug.Log($"XXX: Num of MapObjects: {mapObjects.Count()}");
+
         for (int i = 0; i < numScreens; i++)
         {
-
+            var mapObjectsInTheScreen = MapObjectsInTheScreenByIndex(mapObjects, i);
+            var screenController = new ScreenController(i, mapObjectsInTheScreen);
+            screenControllers.Add(screenController);
         }
+    }
+
+    List<MapObjectController> MapObjectsInTheScreenByIndex(List<MapObjectController> mapObjects, int screenIndex)
+    {
+        float minX = screenIndex * screenUnits;
+        float maxX = minX + screenUnits;
+
+        Debug.Log($"XXX: index: {screenIndex}, X:({minX},{maxX})");
+
+        return mapObjects.Where(e => e.transform.position.x > minX && e.transform.position.x < maxX).ToList();
+    }
+
+    public void ActivateObjectsInScreenByIndex(string side, int screenIndex)
+    {
+        screenControllers.ForEach(e => e.StopMapObjects());
+        screenControllers[screenIndex].StartMapObjects(side);
     }
 }
