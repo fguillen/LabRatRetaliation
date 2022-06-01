29dd231d86c6f0a582af38be1544dc053d50dbde
Fernando Guillen <fguillen.mail@gmail.com>
2022-06-01 15:02:12 +0200

    Basic animations

---
+++ Assets/PlayerFlipDirectionController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFlipDirectionController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (
+            (rb.velocity.x < -1 && transform.localScale.x > 0) ||
+            (rb.velocity.x > 1 && transform.localScale.x < 0)
+        )
+        {
+            Debug.Log($"XXX: {rb.velocity.x}");
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+  }
+}
